9013f926dc61b33548fda43897dee7abb962c365
Wighawag <wighawag@gmail.com>
2020-10-09 21:41:41 +0100

    rename to sourceInfo

--- src/type-extensions.d.ts
+++ src/type-extensions.d.ts
@@ -1,6 +1,6 @@
 import '@nomiclabs/buidler/types';
 declare module '@nomiclabs/buidler/types' {
-  export type LinePreprocessor = (line: string, fileInfo: {absolutePath: string}) => string;
+  export type LinePreprocessor = (line: string, sourceInfo: {absolutePath: string}) => string;
 
   export interface BuidlerConfig {
     preprocess?: {
